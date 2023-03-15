2ddd912ca6de201ce0a9e653cc8ae7202d577150
Jiri Kralovec <jiri@solar.dk>
2023-03-15 20:03:00 +0100

    init

---
+++ scripts/tailwind.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+            }
+        },
+        fontFamily: {
+            'title': ['Transformers', 'sans-serif'],
+            'marker': [ 'Permanent Marker', 'sans-serif' ],
+            'body': ['Segoe UI', 'sans-serif'],
+        }
+    }
+}
